12aaadaf2142a89b4bba156ee475907965eded35
prashanthDev009 <prashanthdev009@gmail.com>
2023-12-26 16:47:01 +0530

    Added the react-type-animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
-// import Typed from "react-typed";
+import { TypeAnimation } from "react-type-animation";
 
 const Header = () => {
   return (
@@ -11,15 +11,22 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello {"👋"} I'm</h5>
         <h3>Prashanth</h3>
-        {/* <h2 className="text-light">
-          <Typed
-            strings={["A Fullstack Developer"]}
-            typeSpeed={70}
-            backSpeed={40}
-            loop
+        <h2 className="text-light">
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "A Fullstack Developer",
+              500, // wait 1s before replacing "MobileDev"
+              "Mobile Developer",
+              500,
+              "UI/UX Designer",
+              500,
+            ]}
+            wrapper="span"
+            speed={60}
+            repeat={Infinity}
           />
-        </h2> */}
-        <h2>A Fullstack Developer</h2>
+        </h2>
         <CTA />
         <HeaderSocials />
 
